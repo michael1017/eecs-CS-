176a1730b91a2dc2508a17a6b6827939aa72a242
Michael Huang <michael881017@gmail.com>
2018-11-19 11:40:03 +0800

    .gitignore modify

---
+++ nqueens.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int board[10]={0};
+int cases = 0;
+int input;
+void conut(int ,int );
+int main(void){
+    
+    scanf(" %d",&input);
+
+}
+void count(int queens, int start_place){
+    int i,j;
+    if(queens == 0 ){
+        cases++;
+        return ;
+    }
+    
+}
